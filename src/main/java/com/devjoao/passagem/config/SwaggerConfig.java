76f4959de22dc517f52098edc137762cd174fa83
João <joaociardullo@hotmail.com>
2024-09-25 00:13:17 -0300

    config swagger

---
+++ src/main/java/com/devjoao/passagem/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package com.devjoao.passagem.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(info = @Info(title = "Memories Club", description = "documentacao da api para realizar o cadastro de clientes da empresa memories club"))
+public class SwaggerConfig {
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("public-apis")
+                .pathsToMatch("/passagem/**")
+                .build();
+    }
+}
